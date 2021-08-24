52f759dfc01f70a1d3817e41230fe21235d94aa4
Subho Ghose <subhoghose.cse@gmail.com>
2021-08-23 21:14:18 -0600

    First commit

---
+++ MathsComponent/MathComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathsComponentNameSpace
+{
+    public class MathsComponent
+    {
+        public int Add(int num1, int num2) {
+            return num1+num2; //Addition
+        }
+
+        public int Substract(int num1, int num2)
+        {
+            return num1 - num2; //Substraction
+        }
+    }
+}
---
+++ UnitTestProject/UnitTestMath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathsComponentNameSpace;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTestMath
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            MathsComponent obj = new MathsComponent();
+            int result = obj.Add(10, 10); 
+            Assert.AreEqual(20, result); // Checking the expected result
+        }
+
+        [TestMethod]
+        public void TestSubstract()
+        {
+            MathsComponent obj = new MathsComponent();
+            int result = obj.Substract(10, 10); 
+            Assert.AreEqual(0, result); // Checking the expected result
+        }
+    }
+}
